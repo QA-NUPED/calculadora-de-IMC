6eb6d7a9db9468525a842a3c4f1dceb415de7881
Gleidison Nascimento | Stone <gleidison.nascimento@stone.com.br>
2023-08-11 18:31:27 -0300

    teste sonar

--- test/levelBMI.test.ts
+++ test/levelBMI.test.ts
@@ -1,48 +1,52 @@
 import { getBMICategory } from '../src/levelBMI'
 
-describe("categoryBMI function", () => {
+describe("getBMICategory function", () => {
     
     it("returns 'Abaixo do Peso' for BMI less than 18.5", () => {
         // Arrenge  
-        const bmi = 18;
+        const bmi = 18.4;
+        const expectedBMI = "Abaixo do Peso"
 
         // Act  
         const result = getBMICategory(bmi);
 
         // Assert
-        expect(result).toBe("Abaixo do Peso");
+        expect(result).toBe(expectedBMI);
     });
   
     it("returns 'Peso Normal' for BMI between 18.5 and 24.9", () => {
         // Arrenge   
         const bmi = 20.5;
+        const expectedBMI = "Peso Normal";
 
-        // Act   
+        // Act  
         const result = getBMICategory(bmi);
 
-        // Assert   
-        expect(result).toBe("Peso Normal");
+        // Assert
+        expect(result).toBe(expectedBMI);
     });
   
     it("returns 'Sobrepeso' for BMI between 25 and 29.9", () => {
         // Arrenge   
         const bmi = 27.4;
+        const expectedBMI = "Sobrepeso"
 
-        // Act   
+        // Act  
         const result = getBMICategory(bmi);
 
         // Assert
-        expect(result).toBe("Sobrepeso");
+        expect(result).toBe(expectedBMI);
     });
   
     it("returns 'Obeso' for BMI equal to or greater than 30", () => {
         // Arrenge   
-        const bmi = 31;
+        const bmi = 30;
+        const expectedBMI = "Obeso"
 
-        // Act   
+        // Act  
         const result = getBMICategory(bmi);
 
         // Assert
-        expect(result).toBe("Obeso");
+        expect(result).toBe(expectedBMI);
     });
 });
